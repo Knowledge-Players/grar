320885393e4141aa1f8d92e8b0584909e22eb047
JbIPS <jeanbaptiste.richardet@gmail.com>
2013-01-29 13:25:40 +0100

    Cleanup TextItem

---
+++ src/com/knowledgeplayers/grar/display/part/TextItemDisplay.hx
@@ -0,0 +1,25 @@
+package com.knowledgeplayers.grar.display.part;
+
+import haxe.xml.Fast;
+
+class TextItemDisplay {
+    public var content (default, default): String;
+    public var tag (default, default): String;
+
+    public function new(xml: Fast = null, content: String = "", tag: String = "")
+    {
+        if(xml != null){
+            this.content = xml.att.Content;
+            this.tag = xml.att.Tag;
+        }
+        else{
+            this.content = content;
+            this.tag = tag;
+        }
+    }
+
+    public function hasVertivalFlow(): Bool
+    {
+        return false;
+    }
+}
