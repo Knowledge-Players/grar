2117b03f5380fb5d23829feb14622816d2894fcf
kevglx <kevglx@gmail.com>
2013-02-05 10:22:00 +0100

    add DataLoader

---
+++ src/com/knowledgeplayers/grar/util/DataLoader.hx
@@ -0,0 +1,10 @@
+package com.knowledgeplayers.grar.util;
+
+import nme.Assets;
+import nme.net.URLLoader;
+import nme.net.URLRequest;
+import nme.net.URLLoaderDataFormat;
+import nme.events.IOErrorEvent;
+import nme.events.Event;
+import nme.events.EventDispatcher;
+
