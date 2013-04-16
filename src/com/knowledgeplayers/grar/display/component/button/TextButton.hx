9a874bcdc622d583989d96832914affb91761872
JbIPS <jeanbaptiste.richardet@gmail.com>
2013-04-16 14:18:29 +0200

    Fix text superposition in buttons

--- src/com/knowledgeplayers/grar/display/component/button/TextButton.hx
+++ src/com/knowledgeplayers/grar/display/component/button/TextButton.hx
@@ -16,11 +16,11 @@ import com.knowledgeplayers.grar.display.text.StyledTextField;
  */
 
 class TextButton extends CustomEventButton {
-    private var text:String;
-    private var textSprite:Sprite;
-    private var styleSheet:String;
+	private var text:String;
+	private var textSprite:Sprite;
+	private var styleSheet:String;
 
-    /**
+	/**
      * Constructor
      * @param	tilesheet : UI tilesheet
      * @param	tile : Tile containing the button
@@ -28,84 +28,81 @@ class TextButton extends CustomEventButton {
      * @param	stylesheet : Style sheet for the text
      */
 
-    public function new(tilesheet:TilesheetEx, tile:String, ?eventName:String, ?_styleSheet:String,?_animations:Hash<AnimationDisplay>)
-    {
-        super(tilesheet, tile, (eventName == null ? "next" : eventName),_animations);
+	public function new(tilesheet:TilesheetEx, tile:String, ?eventName:String, ?_styleSheet:String, ?_animations:Hash<AnimationDisplay>)
+	{
+		super(tilesheet, tile, (eventName == null ? "next" : eventName), _animations);
 
-        styleSheet = _styleSheet;
-        if(eventName == null)
-            propagateNativeEvent = true;
-    }
+		styleSheet = _styleSheet;
+		if(eventName == null)
+			propagateNativeEvent = true;
+	}
 
-    /**
+	/**
      * Setter of the text
      * @param	text : Text to set
      */
 
-    public function setText(text:String):Void
-    {
-        var previousStyleSheet = null;
-        if(styleSheet != null){
-            previousStyleSheet = StyleParser.currentStyleSheet;
-            StyleParser.currentStyleSheet = styleSheet;
-        }
+	public function setText(text:String):Void
+	{
+		if(textSprite != null && contains(textSprite))
+			removeChild(textSprite);
+		var previousStyleSheet = null;
+		if(styleSheet != null){
+			previousStyleSheet = StyleParser.currentStyleSheet;
+			StyleParser.currentStyleSheet = styleSheet;
+		}
 
-        textSprite = new Sprite();
-        var offSetY:Float = 0;
-        var isFirst:Bool = true;
+		textSprite = new Sprite();
+		var offSetY:Float = 0;
+		var isFirst:Bool = true;
 
-        for(element in KpTextDownParser.parse(text)){
-            element.style = className;
-            var padding = StyleParser.getStyle(element.style).getPadding();
-            var item = element.createSprite(width - padding[1] - padding[3]);
+		for(element in KpTextDownParser.parse(text)){
+			element.style = className;
+			var padding = StyleParser.getStyle(element.style).getPadding();
+			var item = element.createSprite(width - padding[1] - padding[3]);
 
-            if(isFirst){
-                offSetY += padding[0];
-            }
-            item.x = padding[3];
-            item.y = offSetY;
-            offSetY += item.height;
+			if(isFirst){
+				offSetY += padding[0];
+			}
+			item.x = padding[3];
+			item.y = offSetY;
+			offSetY += item.height;
 
-            textSprite.addChild(item);
+			textSprite.addChild(item);
 
-        }
+		}
 
-        textSprite.y = -textSprite.height / 2;
+		textSprite.y = -textSprite.height / 2;
 
-        /*textSprite.graphics.beginFill(0);
+		/*textSprite.graphics.beginFill(0);
         textSprite.graphics.drawRect(0,0,textSprite.width, textSprite.height);
         textSprite.graphics.endFill();*/
 
-        #if flash
+		#if flash
             if(StyleParser.getStyle(className).getAlignment() == TextFormatAlign.CENTER)
         #else
-        if(StyleParser.getStyle(className).getAlignment() == "CENTER")
-            #end
-            textSprite.x = -width / 2;
-        else
-            #if flash
+		if(StyleParser.getStyle(className).getAlignment() == "CENTER")
+			#end
+			textSprite.x = -width / 2;
+		else
+			#if flash
             if(StyleParser.getStyle(className).getAlignment() == TextFormatAlign.RIGHT)
         #else
-        if(StyleParser.getStyle(className).getAlignment() == "RIGHT")
-            #end
-            textSprite.x = width / 2 - textSprite.width;
-        else
-            #if flash
+		if(StyleParser.getStyle(className).getAlignment() == "RIGHT")
+			#end
+			textSprite.x = width / 2 - textSprite.width;
+		else
+			#if flash
             if(StyleParser.getStyle(className).getAlignment() == TextFormatAlign.LEFT)
         #else
-        if(StyleParser.getStyle(className).getAlignment() == "LEFT")
-            #end
-            textSprite.x = -width / 2;
+		if(StyleParser.getStyle(className).getAlignment() == "LEFT")
+			#end
+			textSprite.x = -width / 2;
 
-        if(previousStyleSheet != null)
-            StyleParser.currentStyleSheet = previousStyleSheet;
+		if(previousStyleSheet != null)
+			StyleParser.currentStyleSheet = previousStyleSheet;
 
-        if(!contains(textSprite))
-            addChild(textSprite);
-
-
-
-    }
-
+		addChild(textSprite);
+	}
 
 }
