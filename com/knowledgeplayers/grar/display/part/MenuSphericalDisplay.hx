f1e569e9d15304e63b243b8a4c98adabb4f87ee8
JbIPS <jeanbaptiste.richardet@gmail.com>
2013-09-03 17:35:35 +0200

    End spherical menu. T444-0

--- com/knowledgeplayers/grar/display/part/MenuSphericalDisplay.hx
+++ com/knowledgeplayers/grar/display/part/MenuSphericalDisplay.hx
@@ -9,6 +9,8 @@ import com.knowledgeplayers.grar.util.Curve;
 import nme.geom.Point;
 import haxe.xml.Fast;
 
+using com.knowledgeplayers.grar.util.MathUtils;
+
 /**
  * Display of a spherical menu
  */
@@ -16,6 +18,7 @@ class MenuSphericalDisplay extends MenuDisplay {
 
 	private var curves: Map<DisplayObject, Curve>;
 	private var originCurve: Curve;
+	private var initialized: Bool = false;
 
 	public function new(_width:Float, _height:Float)
 	{
@@ -25,25 +28,27 @@ class MenuSphericalDisplay extends MenuDisplay {
 
 	override private function createMenuLevel(level:Xml):Void
 	{
-		if(!levelDisplays.exists(level.nodeName))
-			throw "Display not specified for tag " + level.nodeName;
+		if(!initialized){
+			if(!levelDisplays.exists(level.nodeName))
+				throw "Display not specified for tag " + level.nodeName;
 
-		var fast:Fast = levelDisplays.get(level.nodeName);
+			var fast:Fast = levelDisplays.get(level.nodeName);
 
-		var curve: Curve = null;
-		if(level.nodeName == "hr"){
-			addLine(fast);
-		}
-		else{
-			var tree = new XmlTree(GameManager.instance.game.menu);
-			var i = 0;
-			var nodes = tree.getDepth(i);
-			while(!nodes.isEmpty()){
-				for(level in nodes)
-					createSphericLevel(level);
-				nodes = tree.getDepth(++i);
-				originCurve = null;
+			var curve: Curve = null;
+			if(level.nodeName == "hr"){
+				addLine(fast);
 			}
+			else{
+				var tree = new XmlTree(GameManager.instance.game.menu);
+				var i = 0;
+				var nodes = tree.getDepth(i);
+				while(!nodes.isEmpty()){
+					for(level in nodes)
+						createSphericLevel(level);
+					nodes = tree.getDepth(++i);
+				}
+			}
+			initialized = true;
 		}
 	}
 
@@ -53,28 +58,35 @@ class MenuSphericalDisplay extends MenuDisplay {
 		var button = addButton(fast.node.Button, GameManager.instance.getItemName(level.value.get("id")));
 		buttons.set(level.value.get("id"), button);
 		var curve: Curve;
+		var parent: DisplayObject = buttons.get(level.parent.value.get("id"));
 		if(curves.exists(buttons.get(level.parent.value.get("id")))){
 			curve = curves.get(buttons.get(level.parent.value.get("id")));
 		}
-		else if(originCurve == null){
-			var parent: DisplayObject = buttons.get(level.parent.value.get("id"));
-			if(parent == null){
+		else if(originCurve == null && parent == null){
 				var x = fast.has.x ? Std.parseFloat(fast.att.x) : 0;
 				var y = fast.has.y ? Std.parseFloat(fast.att.y) : 0;
 				originCurve = new Curve(new Point(x, y), Std.parseFloat(fast.att.radius));
 				curve = originCurve;
 				if(fast.has.minAngle) originCurve.minAngle = Std.parseFloat(fast.att.minAngle);
 				if(fast.has.maxAngle) originCurve.maxAngle = Std.parseFloat(fast.att.maxAngle);
-			}
-			else{
-				curve = new Curve(new Point(parent.x, parent.y), Std.parseFloat(fast.att.radius));
-				if(fast.has.minAngle) curve.minAngle = Std.parseFloat(fast.att.minAngle);
-				if(fast.has.maxAngle) curve.maxAngle = Std.parseFloat(fast.att.maxAngle);
-				curves.set(parent, curve);
-			}
 		}
-		else
+		else if(parent == null)
 			curve = originCurve;
+		else{
+			curve = new Curve(new Point(parent.x, parent.y), Std.parseFloat(fast.att.radius));
+			var angle: Float = -1;
+			for(c in curves){
+				if(c.contains(parent))
+					angle = c.getAngle(parent);
+			}
+			if(angle == -1)
+				angle = originCurve.getAngle(parent);
+			var offset = angle.radToDegree() - Std.parseFloat(fast.att.maxAngle)/2;
+			curve.minAngle = (fast.has.minAngle ? Std.parseFloat(fast.att.minAngle) : 0) + offset;
+			curve.maxAngle = (fast.has.maxAngle ? Std.parseFloat(fast.att.maxAngle) : 0) + offset;
+			trace(curve.minAngle, curve.maxAngle);
+			curves.set(parent, curve);
+		}
 
 		curve.add(button);
 		addChild(button);
--- com/knowledgeplayers/grar/util/Curve.hx
+++ com/knowledgeplayers/grar/util/Curve.hx
@@ -1,5 +1,6 @@
 package com.knowledgeplayers.grar.util;
 
+import com.knowledgeplayers.grar.util.MathUtils;
 import haxe.ds.GenericStack;
 import nme.display.DisplayObject;
 import nme.geom.Point;
@@ -26,8 +27,8 @@ class Curve {
 	public var radius (default, default):Float;
 
 	private var objects: Array<DisplayObject>;
+	//private var objToAngles : Array<{obj: DisplayObject, angle: Float}>;
 	private var objToAngles : Map<DisplayObject, Float>;
-	private var satellites: Map<DisplayObject, GenericStack<DisplayObject>>;
 
 	public function new(center: Point, radius: Float = 1, minAngle: Float = 0, maxAngle: Float = 360)
 	{
@@ -38,17 +39,18 @@ class Curve {
 
 		objects = new Array<DisplayObject>();
 		objToAngles = new Map<DisplayObject, Float>();
-		satellites = new Map<DisplayObject, GenericStack<DisplayObject>>();
 	}
 
 	public function add(object:DisplayObject, withTween: Bool = true):DisplayObject
 	{
 		objects.push(object);
+		//objToAngles = new Array<{obj: DisplayObject, angle: Float}>();
 		var angle = (maxAngle - minAngle)/(2*objects.length);
 		for(i in 0...objects.length){
-			var a = degreeToRad(angle+(angle*2*i)+minAngle);
+			var a = MathUtils.degreeToRad(angle+(angle*2*i)+minAngle);
 			objects[i].x = Math.cos(a)*radius + center.x;
 			objects[i].y = Math.sin(a)*radius + center.y;
+			//objToAngles.push({obj: objects[i], angle: a});
 			objToAngles.set(objects[i], a);
 		}
 		return object;
@@ -56,7 +58,31 @@ class Curve {
 
 	public function getAngle(obj: DisplayObject): Float
 	{
-		return objToAngles.get(obj);
+		/*if(objToAngles == null)
+			throw "[Curve] This curve is empty.";
+		var i = 0;
+		while(i < objToAngles.length && objToAngles[i].obj != obj)
+			i++;
+		if(i == objToAngles.length){
+			trace("Item '"+obj+"' not found.");
+			return 0;
+		}
+		else
+			return objToAngles[i].angle;*/
+		return objToAngles[obj];
+	}
+
+	public function contains(obj: DisplayObject): Bool
+	{
+		/*if(objToAngles == null)
+			throw "[Curve] This curve is empty.";
+		var i = 0;
+		while(i < objToAngles.length && objToAngles[i].obj != obj)
+			i++;
+
+		return i != objToAngles.length;
+		*/
+		return objToAngles.exists(obj);
 	}
 
 	public function flush():Void
@@ -69,12 +95,5 @@ class Curve {
 	public function toString():String
 	{
 		return 'Curve: center ('+center.x+';'+center.y+') radius '+radius+' angles '+minAngle+'-'+maxAngle+' '+objects.length+' children';
-	}
-
-	// Privates
-
-	private inline function degreeToRad(degree: Float): Float
-	{
-		return degree * Math.PI/180;
 	}
 }
